ed30e02211c771fc0d742baa6e287aae7243be6e
Andrei Lemdyanov <lemdyanov5@mail.ru>
2017-03-02 01:27:20 +0400

    version 0.2

--- Calculate.java
+++ Calculate.java
@@ -5,5 +5,14 @@ System.out.println("Calculate...");
 int first = Integer.parseInt(arg[0]);
 int second = Integer.parseInt(arg[1]);
 int summ = first + second;
-System.out.println("Summ " + summ); }
+long third = Long.parseLong(arg[2]);
+double fourth = Double.parseDouble(arg[3]);
+double f = third/fourth;
+double fifth = f * f;
+
+System.out.println("Summ " + summ);
+System.out.println("now " + summ * third);
+System.out.println("after " +  f);
+System.out.println("final " + fifth);
+}
 }
