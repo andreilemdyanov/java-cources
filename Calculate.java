1fafd69814e35304f8bfd8f1b4606ea3189189c3
Andrei Lemdyanov <lemdyanov5@mail.ru>
2017-03-02 01:22:01 +0400

    New version

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,9 @@
 public class Calculate {
 
-public static void main(String[] args){
-System.out.println("Calculate..."); }
+public static void main(String[] arg){
+System.out.println("Calculate...");
+int first = Integer.parseInt(arg[0]);
+int second = Integer.parseInt(arg[1]);
+int summ = first + second;
+System.out.println("Summ " + summ); }
 }
